84d6e77ce37f9880ca254ba07af14b536f05d17a
kellycatchan <147563023+kellycatchan@users.noreply.github.com>
2025-08-06 16:34:23 -0400

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -8,7 +8,6 @@ const map = L.map('map', {
   boxZoom: false,
   keyboard: false,
   dragging: true,
-  touchZoom: true,
 });
 
 const esriWorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
