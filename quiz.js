31e60fc8694828c1166a14059f42231cba5f6140
kellycatchan <147563023+kellycatchan@users.noreply.github.com>
2025-08-14 13:58:55 -0400

    Update quiz.js
    max zoom 32

--- quiz.js
+++ quiz.js
@@ -12,7 +12,7 @@ const map = L.map('map', {
 
 const esriWorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
 	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
-	maxZoom: 8
+	maxZoom: 32
 });
 
 esriWorldGrayCanvas.addTo(map);
